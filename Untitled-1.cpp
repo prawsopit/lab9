d68b3bce78274c1d88ad2bf2f69bf24c4c7d01ce
prawsopit <prawsopit@gmail.com>
2022-12-29 13:48:02 +0700

    9.5

---
+++ Untitled-1.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double age, height, bounty;
+    cout << "Enter your age: ";
+    cin >> age;
+    
+
+    if (age <= 25)
+    {
+        cout << "Enter your height: ";
+        cin >> height;
+        if (height < 100)
+        {
+            cout << "Your character = Chopper";
+        }
+        else if (height < 180)
+        {
+            cout << "Your character = Usopp";
+        }
+        else
+        {
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+            if (bounty > 1100000000)
+            {
+                cout << "Your character = Zoro";
+            }
+            else
+            {
+                cout << "Your character = Sanji";
+            }
+        }
+    }
+    else if (age <= 60)
+    {
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+        if (bounty > 500000000)
+        {
+            cout << "Your character = Jinbe";
+        }
+        else
+        {
+            cout << "Your character = Franky";
+        }
+    }
+    else
+    {
+        cout << "Your character = Brook";
+    }
+}
